a8648e316819c2bc86e6fa9868379f386e96e7d1
shubhampal077 <shubhampalggps@gmail.com>
2020-03-05 18:15:53 +0530

    second commit

---
+++ stockprediction.py
@@ -0,0 +1,40 @@
+# Import
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+# Import data
+data = pd.read_csv('myproject.csv')
+
+# Drop date variable
+data = data.drop(['DATE'], 1)
+
+# Dimensions of dataset
+n = data.shape[0]
+p = data.shape[1]
+
+# Make data a np.array
+data = data.values
+
+# Training and test data
+train_start = 0
+train_end = int(np.floor(0.8*n))
+test_start = train_end + 1
+test_end = n
+data_train = data[np.arange(train_start, train_end), :]
+data_test = data[np.arange(test_start, test_end), :]
+
+# Scale data
+scaler = MinMaxScaler(feature_range=(-1, 1))
+scaler.fit(data_train)
+data_train = scaler.transform(data_train)
+data_test = scaler.transform(data_test)
+
+# Build X and y
+X_train = data_train[:, 1:]
+y_train = data_train[:, 0]
+X_test = data_test[:, 1:]
+y_test = data_test[:, 0]
+
