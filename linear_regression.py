94f9e24953ebae82f8aa766be1c809db67fc28c2
harshil <harshilsharma1997@gmail.com>
2020-05-26 18:27:28 +0530

    Modifying linear_regression file

--- linear_regression.py
+++ linear_regression.py
@@ -11,49 +11,53 @@ plt.style.use('fivethirtyeight')
 #Importing the dataset
 df = pd.read_csv('google.csv') 
 
-#Setting index as date and date format
-df['Date'] = pd.to_datetime(df.Date,format='%Y-%m-%d')
-df.index = df['Date']
-
 print(df.head(25))
 
-df.describe()
+import seaborn as sns;
+sns.set(style="ticks", color_codes=True)
 
-x = df[['Open','High','Low','Volume']].values
+sns.pairplot(df,x_vars=['Open'],y_vars='Close',height=10,aspect=1.5,kind='reg',plot_kws={'line_kws':{'color':'red' ,'linewidth':'2'}})
+
+sns.pairplot(df,x_vars=['Low'],y_vars='High',height=10,aspect=1.5,kind='reg',plot_kws={'line_kws':{'color':'red' ,'linewidth':'2'}})
+
+sns.pairplot(df, diag_kind="kde", markers="+",
+                 plot_kws=dict(s=50, edgecolor="b", linewidth=1),
+                 diag_kws=dict(shade=True))
+
+print(df.describe())
+
+x = df[['Open','High','Low']].values
 y = df['Close'].values
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.5,random_state=0)
 
-#Visualize the closing price history
 plt.figure(figsize=(16,8))
-plt.title('Close Price History')
+plt.title('Close Price History',fontsize=18)
 plt.plot(df['Close'],linewidth=2)
-plt.xlabel('Date',fontsize=18)
+plt.xlabel('Open Price USD ($)',fontsize=18)
 plt.ylabel('Close Price USD ($)',fontsize=18)
 plt.show()
 
 regressor = LinearRegression()
 
-regressor.fit(x_train,y_train)
+z=regressor.fit(x_train,y_train)
 
 print(regressor.coef_)
 
 y_pred = regressor.predict(x_test)
 
 result = pd.DataFrame({'Actual':y_test.flatten(),'Predicted':y_pred.flatten()})
+print(result.head(25))
 
-result.head(25)
+z.score(x_test, y_test)
 
 print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
 print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
 print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
-#graph=df.head(20)
-#graph.plot(kind='bar')
-
 plt.figure(figsize=(16,8))
-plt.title('Actual Price')
+plt.title('Open Price USD ($)',fontsize=18)
 plt.plot(result['Actual'])
-# plt.plot(result['Predicted'])
+plt.plot(result['Predicted'])
 # plt.plot(y_test)
 # plt.plot(y_pred)
 
@@ -61,10 +65,27 @@ plt.plot(result['Actual'])
 # plt.ylabel('Close',fontsize=18)
 
 plt.figure(figsize=(16,8))
-plt.title('Predicted Price')
+plt.title('Predicted Price USD ($)',fontsize=18)
 plt.plot(result['Actual'])
+plt.show()
 
-plt.figure(figsize=(16,16))
-plt.title('Actual and predicted Price')
+plt.figure(figsize=(16,8))
+plt.title('Actual and predicted Price USD ($)',fontsize=18)
 plt.plot(result['Actual'])
 plt.plot(result['Predicted'])
+plt.show()
+
+plt.figure(figsize=(16,8))
+plt.title('Actual and predicted Price USD ($)',fontsize=18)
+plt.scatter(result.Actual,result.Predicted)
+plt.xlabel('Actual Price USD ($)',fontsize=18)
+plt.ylabel('Close  Price USD ($)',fontsize=18)
+plt.show()
+
+sns.pairplot(result,x_vars=['Actual'],y_vars='Predicted',height=10,aspect=1.5,kind='reg',plot_kws={'line_kws':{'color':'red' ,'linewidth':'2'}})
+
+m= df[['Open','High','Low']].values
+y_pred2 = regressor.predict(m)
+print('Prediction: '),
+y_pred2
+
